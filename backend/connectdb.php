17c390097586786dc32ee2bba3630ea45728f9dd
raghavendra.ns <raghunsp663@gmail.com>
2016-08-17 11:43:23 +0530

    first commit

--- backend/connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HOSTNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "root");
-define("DATABASE", "eventsDB");
-$dbhandle= new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("unable to connect");
-
-?>
